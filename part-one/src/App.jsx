fd050262403145047f8cddf18e3cf72ea8b105f5
Wisdom Kara <wiskara1@gmail.com>
2026-05-01 18:36:24 +0100

    i now used an object for the new data
    I've refactored the data model from separate scalar variables (part1, part2, part3) into a single structured array of objects (parts). Each object now groups related data (name and exercises) together, which makes the data more scalable and easier to manage. However, the components must be updated to consume this new array structure instead of individual props.

--- part-one/src/App.jsx
+++ part-one/src/App.jsx
@@ -2,44 +2,40 @@ const Header=(props)=>{
   return <h1>{props.course}</h1>
 }
 
-const Content = ({part1,exercises1,part2,exercises2,part3,exercises3}) => {
+const Content = ({parts}) => {
   return (
     <>
-      <p>{part1}: {exercises1}</p>
-      <p>{part2}: {exercises2}</p>
-      <p>{part3}: {exercises3}</p>
+   <p>{parts[0].name} {parts[0].exercises}</p>
+   <p>{parts[1].name} {parts[1].exercises}</p>
+   <p>{parts[2].name} {parts[2].exercises}</p>
     </>
   )
 }
 
-const Total = ({exercises1,exercises2,exercises3}) => {
+const Total = ({parts}) => {
   return (
-    <p>Total exercises: {exercises1 + exercises2 + exercises3}</p>
+  <p>
+    Total Exercises:{parts[0].exercises + parts[1].exercises + parts[2].exercises}
+  </p>
   )
 }
 const App = () => {
- 
 
   const course="Half Stack application development"
-  const part1="Fundamentals of React"
-  const exercises1=10
-  const part2="Using props to pass data"
-  const exercises2=7
-  const part3="State of a component"
-  const exercises3=14
+  const parts=[
+{name:"Fundamentals of React",exercises:10},
+{name:"Using props to pass data",exercises:7},
+{name:"State of a component",exercises:14}
+
+  ]
+ 
   return(
     <>
     <Header course={course}/>
-    <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-    <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    <Content parts={parts}/>
+    <Total parts={parts}/>
 
-   
-     
     </>
-
-    
   )
- 
 }
-                
 export default App

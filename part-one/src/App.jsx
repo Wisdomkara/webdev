6e81fd98490b84fa4911b5a5955cd90076f3c0e6
Wisdom Kara <wiskara1@gmail.com>
2026-08-05 00:37:16 +0100

    Update App.jsx

--- part-one/src/App.jsx
+++ part-one/src/App.jsx
@@ -22,7 +22,7 @@ const Total = ({parts}) => {
 const App = () => {
 
   const course="Half Stack application development"
-  const parts=[
+  const parts=[ 
 {name:"Fundamentals of React",exercises:10},
 {name:"Using props to pass data",exercises:7},
 {name:"State of a component",exercises:14}
